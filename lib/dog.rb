97e4d0bd61b0982dba11b3b494b8932a7405302d
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-16 00:17:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(new_name)
+    @name = "Fido"
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(new_breed)
+    @breed = "Beagle"
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(new_name)
+    @name = "Beyonce"
+  end
+
+  def name
+    @name
+  end
+
+  def job=(new_job)
+    @job = "Singer"
+  end
+
+  def job
+    @job
+  end
+end
